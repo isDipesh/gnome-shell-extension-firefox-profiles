3b546c742294d6f372523cf6d835e4b36d4d79c3
Dipesh Acharya <xtranophilist@gmail.com>
2012-05-31 03:54:07 +0545

    updated to work with gnome 3.4

--- extension.js
+++ extension.js
@@ -15,7 +15,6 @@ FirefoxProfiles.prototype = {
     _init: function() {
         PanelMenu.SystemStatusButton.prototype._init.call(this, 'start-here');
         this._button = new St.Button();
-        this._button.set_tooltip_text('Click me to choose a Firefox Profile!');
         this._button.set_child(new St.Icon({
             icon_name: 'firefox',
             icon_type: St.IconType.FULLCOLOR,
@@ -32,7 +31,7 @@ let firefoxProfiles;
 function enable() {
     firefoxProfiles = new FirefoxProfiles();
     let _children = Main.panel._leftBox.get_children();
-    Main.panel._leftBox.insert_actor(firefoxProfiles._button, _children.length - 1);
+    Main.panel._leftBox.insert_child_at_index(firefoxProfiles._button, _children.length - 1);
 }
 
 function disable() {
